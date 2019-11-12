2ade20bec4dfe5a6ae855e6216ffde199772bf4d
0xx012345 <56978634+0xx012345@users.noreply.github.com>
2019-11-12 23:41:39 +0300

    duzeltmeler yapildi
    duzeltmeler yapildi

--- src/main/java/proje/v1/api/service/student/StudentService.java
+++ src/main/java/proje/v1/api/service/student/StudentService.java
@@ -1,8 +1,10 @@
 package proje.v1.api.service.student;
 
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import proje.v1.api.domian.classroom.Classroom;
+import proje.v1.api.domian.classroom.ClassroomRepository;
 import proje.v1.api.domian.rollcall.RollCall;
 import proje.v1.api.domian.student.Student;
 import proje.v1.api.domian.student.StudentRepository;
@@ -15,7 +17,8 @@ public class StudentService {
 
     @Autowired
     StudentRepository studentRepository;
-
+    @Autowired
+    ClassroomRepository classroomRepository;
     public Student save(Student student) {
         return studentRepository.save(student);
     }
@@ -33,5 +36,21 @@ public class StudentService {
     public Student findByFingerMark(String hashedFingerMark) {
         return studentRepository.findByFingerMark(hashedFingerMark).
                 orElseThrow(()-> new NotFoundException("Not found any student with fingerMark : "+hashedFingerMark));
+    }
+    public List<Classroom> getClassrooms(String id) {
+        return classroomRepository.findById(id).orElseThrow(() -> new NotFoundException("hata "));
+    }
+    public void deleteStudentById(Long id) {
+
+        boolean status=studentRepository.findById(id).isPresent();
+        if(!status) {
+
+            throw new NotFoundException("silinecek kayit bulunamadi");
+        }
+        studentRepository.deleteById(id);
+        
+    }
+    public Student findById(Long id) {
+        return studentRepository.findById(id);
     }
 }
